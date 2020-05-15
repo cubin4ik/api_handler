6d3ed9bbfa573f81c4089faa79083a1652c95fa3
Suarez Alehandro (DCEE/SLF-OEM-RU) <sua1mk@bosch.com>
2020-05-15 19:08:52 +0300

    major initial code is implemented

---
+++ apihandle.py
@@ -0,0 +1,53 @@
+from urllib import request, parse
+from collections import namedtuple
+from xml.etree import ElementTree
+from xml.parsers import expat
+import json
+
+
+class RequestAPI:
+    """Handles links, parameters and connections to source API"""
+
+    def __init__(self, api, **kwargs):
+        self.api = api
+        self.pars = kwargs
+        self.url = self.build_link()
+        self.resp_raw = self.get_resp()
+
+    def build_link(self):
+        """Returns full url to connect and get data"""
+
+        api_scheme, api_netloc, api_path, api_query, api_fragment = parse.urlsplit(self.api)
+
+        Link = namedtuple('Link', 'scheme, netloc, path, query, fragment')
+        url_pars = Link(scheme=api_scheme,
+                        netloc=api_netloc,
+                        path=api_path,
+                        query=api_query,
+                        fragment=api_fragment)
+
+        url = url_pars._replace(**self.pars)
+        return parse.urlunsplit(url)
+
+    def get_resp(self):
+        """Connects and pulls xml data"""
+
+        data = request.urlopen(self.url)
+        enc = data.headers.get_content_charset()
+
+        return data.read().decode(enc)
+
+    def parse_xml(self):
+        pass
+
+    def parse_json(self):
+        pass
+
+
+# source_api = 'https://jsonplaceholder.typicode.com/comments'
+source_api = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req=02/03/2002'
+par = 'date_req'
+day = '05/03/2002'
+
+my_req = RequestAPI(source_api, query=f'{par}={day}')
+print(my_req.resp_raw)
